199ea33013cb8460de7cee92e3b127096cbd6475
SachiniPinnaduwa <sachinipinnaduwa2@gmail.com>
2025-07-29 09:33:38 +0530

    Creating Authentication APIs (Login, Sign-Up, Get Profile Info, Image Upload)

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -21,6 +23,7 @@ connectDB();
 app.use(express.json());
 
 //Routes
+app.use("/api/auth", authRoutes);
 
 //Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
